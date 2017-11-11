b6b40e1d285275f3f620ec2aa740629c9221c232
damien0587 <damien0587@gmail.com>
2017-11-10 23:20:27 -0600

    initial

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+var friendsArray = [{
+        name: 'Beyonce',
+        photo: 'http://cache.lovethispic.com/uploaded_images/229823-Black-And-White-Beyonce-Picture.jpg',
+        scores: [
+            '1',
+            '5',
+            '5',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'SZA',
+        photo: 'http://thatgrapejuice.net/wp-content/uploads/2017/06/sza-instagram-640x579.jpg',
+        scores: [
+            '2',
+            '4',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '2',
+            '1',
+            '5'
+        ]
+    }
+]
